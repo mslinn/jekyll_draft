62efe4ba02445e5e2fd2841637088b4bcfb4acf9
Mike Slinn <mslinn@micronauticsresearch.com>
2022-05-02 17:19:30 -0400

    Fumbling towards ecstasy

--- lib/jekyll_draft.rb
+++ lib/jekyll_draft.rb
@@ -7,36 +7,64 @@ require 'yaml'
 
 # Detects draft documents
 module Jekyll
-  module DraftFilter
-    def is_draft(doc)
-      puts "is_draft called for #{doc.title}"
+  # Define these methods outside of the filter module so they can be invoked externally
+  module Draft
+    def draft?(doc)
+      if doc.nil?
+        puts "Jekyll:Draft.draft? doc is nil!".yellow
+        return 'undefined_doc'
+      end
+
+      # puts "draft? invoked from #{doc.path}"
       return !doc.data['published'] if doc.respond_to?(:data) && doc.data.key?('published')
 
-      return !doc['published'] if doc.respond_to?(:key) && doc.key?('published')
+      return !doc['published'] if doc.key?('published')
 
       return !doc.published if doc.respond_to?(:published)
 
-      return doc['draft'] if doc.respond_to?(:key) && doc.key?('draft')
+      return doc['draft'] if doc.key?('draft')
 
       false
     end
 
     def draft_html(doc)
-      puts "draft_html called for #{doc.title}"
-      return '' unless is_draft(doc)
+      # puts "draft_html invoked from #{doc.path}"
+      return '' unless draft?(doc)
 
       " <i class='bg_light_yellow' style='padding-left: 0.5em; padding-right: 0.5em;'>Draft</i>"
     end
 
-    def root(doc)
-      puts "root called for #{doc.title}"
-      docs = @context.registers[:site].collections[doc.collection].docs
+    def root(doc, site)
+      # puts "root invoked from #{doc.path}"
+      return "/index.html" unless doc.respond_to?(:collection)
+
+      collection_name = doc.collection
+      docs = site.respond_to?(collection_name) ? site[collection_name] : site.collections[collection_name].docs
       index = docs.find { |d| d.url.end_with? "index.html" }
       return index.url if index
 
       docs.min.url
     end
+
+    module_function :draft?, :draft_html, :root
   end
+
+  module DraftFilter
+    def is_draft(doc)
+      Draft::draft?(doc)
+    end
+
+    def draft_html(doc)
+      Draft::draft_html(doc)
+    end
+
+    # DraftFilter.singleton_class.included_modules
+    #   [JSON::Ext::Generator::GeneratorMethods::Object, PP::ObjectMixin, Kernel]
+    def root(doc, site)
+      Draft::root(doc, site)
+    end
+  end
+
   Liquid::Template.register_filter(DraftFilter)
   PluginMetaLogger.instance.info { "Loaded DraftFilter plugin." }
 end
