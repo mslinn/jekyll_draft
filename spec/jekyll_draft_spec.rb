f9addd54675f50a47b8ed6ffd16481f6904ef930
Mike Slinn <mslinn@mslinn.com>
2022-05-03 19:10:35 -0400

    Wrote some tests

--- spec/jekyll_draft_spec.rb
+++ spec/jekyll_draft_spec.rb
@@ -4,31 +4,34 @@ require 'jekyll'
 require 'jekyll_plugin_logger'
 require_relative '../lib/jekyll_draft'
 
-RSpec.describe(Jekyll::Draft) do
+RSpec.describe(Jekyll::Draft) do # rubocop:disable Metrics/BlockLength
   include Jekyll::Draft
 
   let(:page_draft) do
     page_ = double('Jekyll::Page')
-    allow(page_).to receive(:published) { true }
     allow(page_).to receive(:draft) { true }
+    page_
+  end
+  let(:page_not_draft) do
+    page_ = double('Jekyll::Page')
+    allow(page_).to receive(:draft) { false } # lower priority
+    page_
+  end
+  let(:page_published) do
+    page_ = double('Jekyll::Page')
+    allow(page_).to receive(:published) { true } # higher priority
     page_
   end
   let(:page_not_published) do
     page_ = double('Jekyll::Page')
     allow(page_).to receive(:published) { false } # higher priority
-    allow(page_).to receive(:draft) { true } # lower priority
-    page_
-  end
-  let(:page_not_published_or_draft) do
-    page_ = double('Jekyll::Page')
-    allow(page_).to receive(:published) { false } # higher priority
-    allow(page_).to receive(:draft) { false } # lower priority
     page_
   end
 
   it 'detects drafts' do
-    expect(draft?(page_draft)).to be_falsey
+    expect(draft?(page_draft)).to be_truthy
+    expect(draft?(page_not_draft)).to be_falsey
+    expect(draft?(page_published)).to be_falsey
     expect(draft?(page_not_published)).to be_truthy
-    expect(draft?(page_not_published_or_draft)).to be_truthy
   end
 end
