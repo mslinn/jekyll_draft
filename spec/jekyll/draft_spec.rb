b0f0249ac39faa413322be92f83d0efd26643eba
Mike Slinn <mslinn@mslinn.com>
2023-05-27 13:55:14 -0400

    -

--- spec/jekyll/draft_spec.rb
+++ spec/jekyll/draft_spec.rb
@@ -1,11 +1,21 @@
 require 'jekyll'
 require 'jekyll_plugin_logger'
+require 'rspec/match_ignoring_whitespace'
 require_relative '../../lib/jekyll_draft'
+require_relative 'mocks'
 
 RSpec.describe(Jekyll::Draft) do
   include described_class
 
+  let(:logger) do
+    PluginMetaLogger.instance.new_logger(self, PluginMetaLogger.instance.config)
+  end
+
+  let(:parse_context) { TestParseContext.new }
+
   it 'detects drafts' do
-    pending('Not yet')
+    # expect(result).to match_ignoring_whitespace <<-END_RESULT
+    #   <img src="./blah.webp">
+    # END_RESULT
   end
 end
---
+++ spec/jekyll/mocks.rb
@@ -0,0 +1,62 @@
+Registers = Struct.new(:page, :site)
+
+# Mock for Collections
+class Collections
+  def values
+    []
+  end
+end
+
+# Mock for Site
+class SiteMock
+  attr_reader :config
+
+  def initialize
+    @config = YAML.safe_load(File.read('../demo/_config.yml'))
+    @config['env'] = { 'JEKYLL_ENV' => 'development' }
+  end
+
+  def collections
+    Collections.new
+  end
+end
+
+# Mock for Liquid::Context
+class TestLiquidContext < Liquid::Context
+  def initialize
+    super
+
+    page = {
+      "content"     => "blah blah",
+      "description" => "Jekyll draft demo",
+      "dir"         => "/",
+      "excerpt"     => nil,
+      "layout"      => "default",
+      "name"        => "index.html",
+      "path"        => "index.html",
+      "title"       => "Welcome",
+      "url"         => "/",
+    }
+
+    @content = "This is the content"
+    @registers = Registers.new(
+      page,
+      SiteMock.new
+    )
+  end
+end
+
+# Mock for Liquid::ParseContent
+class TestParseContext < Liquid::ParseContext
+  attr_reader :line_number, :registers
+
+  def initialize
+    super
+    @line_number = 123
+
+    @registers = Registers.new(
+      { 'path' => 'path/to/page.html' },
+      SiteMock.new
+    )
+  end
+end
